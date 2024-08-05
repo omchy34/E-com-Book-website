64d22c8503186a794553ad19f9d2dd95f22e4f55
om50 <omchy34@gmail.com>
2024-08-05 22:16:21 +0530

    chnaegs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-com-Book-website/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
