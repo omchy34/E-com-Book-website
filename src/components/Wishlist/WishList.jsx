9c092e832901b5e574b481228f14f43972f456fc
om50 <omchy34@gmail.com>
2024-08-06 13:51:19 +0530

    changes

--- src/components/Wishlist/WishList.jsx
+++ src/components/Wishlist/WishList.jsx
@@ -1,35 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { LoadwishList, Removewishlist } from "../../features/wishlistCard/wishlistSlice.js";
+import { LoadwishList } from "../../features/wishlistCard/wishlistSlice.js";
 import Card from "../Card/Card";
-import './WishList.css'
+import './WishList.css';
 
 const WishList = () => {
   const wishlistItems = useSelector((state) => state.wishlist.items);
   const dispatch = useDispatch();
 
-  function handleRemove() {
-    // dispatch(Removewishlist({ product_id: productId }));
-    // console.log(productId);
-    
-    if(localStorage.getItem("accessToken")) {
-      const accessToken = localStorage.getItem("accessToken");
-      dispatch(setToken({ accessToken, refreshToken: localStorage.getItem("refreshToken") }));
+  useEffect(() => {
+    // Only run this code when the component mounts
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      const refreshToken = localStorage.getItem("refreshToken");
+      dispatch(setToken({ accessToken, refreshToken }));
+
       const storedWishlist = JSON.parse(localStorage.getItem('WishlistItem'));
-      if(storedWishlist){
+      if (storedWishlist) {
         dispatch(LoadwishList(storedWishlist));
       }
     }
-  }
-
-  handleRemove()
+  }, [dispatch]);
 
   return (
     <section className="Main">
       <h1 className="text-center text-red-500 text-2xl pt-5">Wish List</h1>
       <div className="flex flex-wrap gap-5 p-10 justify-center items-center">
         {wishlistItems.map((item) => (
-          <Card key={item._id} product={item} isInWishlist={true}  />
+          <Card key={item._id} product={item} isInWishlist={true} />
         ))}
       </div>
     </section>
