58d9fc5a2ff60df329d2150b19bae8ea585f94d1
Om Choudhary <76719323+omchy34@users.noreply.github.com>
2024-08-09 20:01:30 +0530

    Update Login.jsx

--- src/components/LoginRej/Login.jsx
+++ src/components/LoginRej/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
       console.log('API Response:', response);
 
       if (response.data.success === true) {
-        const { accessToken, refreshToken } = response.data.data;
+        const { accessToken } = response.data.data;
 
         if (accessToken) {
           dispatch(setToken({ accessToken }));
